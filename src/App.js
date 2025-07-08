beab8bac5e1be639ac47b0ffdea08dbf4b286814
harshh46 <harshraval2293@gmail.com>
2025-07-08 16:44:28 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from "./logo.svg";
+import "./App.css";
+// import Hello from "./components/Hello.js";
+import Counter from "./components/Counter.js";
+import Feedback from "./components/Feedback.js";
+
+function App() {
+  return (
+    <div>
+      {/* <Hello /> */}
+      <Counter />
+      <Feedback />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Feedback.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+
+function Feedback() {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  return (
+    <div>
+      <h1>
+        Good: {good} Neutral: {neutral} Bad: {bad}
+      </h1>
+      <button onClick={() => setGood(good + 1)}>Good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
+      <button onClick={() => setBad(bad + 1)}>Bad</button>
+    </div>
+  );
+}
+export default Feedback;
